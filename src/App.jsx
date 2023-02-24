a57f05e587bce1925370fb107dc647460e580b75
Kuba Mrowiec <mruvekss@gmail.com>
2023-02-24 11:23:17 +0100

    Homepage update

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Navbar from './components/Navbar'
+import Home from './components/Home'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="">
       <Navbar />
+      <Home />
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,17 +4,18 @@ const Navbar = () => {
 
   const [nav, setActiveNav] = useState('')
 
-  const cellStyle = 'w-fit h-fit px-2 py-1 bg-transparent hover:text-white hover:cursor-pointer'
+  const cellStyle = 'w-fit h-fit px-2 py-1 bg-transparent hover:cursor-pointer'
+  const navStyle = 'text-black transform scale-125 transition duration-150 ease-in-out'
 
   return (
-    <nav className="flex justify-center gap-10 text-2xl items-center absolute top-0 h-20 w-full uppercase bg-green-400 text-black border-black border-b-4">
+    <nav className="flex justify-center gap-10 text-4xl items-center absolute top-0 h-20 w-full uppercase text-gray-400 border-black border-b-2">
 
       <div id="logo" className="absolute top-0 left-0 border-black border-4 h-20 w-34 " >KILLMEINDUSTRIES</div>
 
-      <div className={`${cellStyle} ${ nav === 'Club' ? 'transform scale-150 transition duration-150 ease-in-out' : null }`} onClick={() => setActiveNav('Club')}>Club</div>
-      <div className={`${cellStyle} ${ nav === 'Events' ? 'transform scale-150 transition duration-150 ease-in-out' : null }`} onClick={() => setActiveNav('Events')}>Events</div>
-      <div className={`${cellStyle} ${ nav === 'Residents' ? 'transform scale-150 transition duration-150 ease-in-out' : null }`} onClick={() => setActiveNav('Residents')}>Residents</div>
-      <div className={`${cellStyle} ${ nav === 'Gallery' ? 'transform scale-150 transition duration-150 ease-in-out' : null }`} onClick={() => setActiveNav('Gallery')}>Gallery</div>
+      <div className={`${cellStyle} ${ nav === 'Club' ? navStyle : null }`} onClick={() => setActiveNav('Club')}>Club</div>
+      <div className={`${cellStyle} ${ nav === 'Events' ? navStyle : null }`} onClick={() => setActiveNav('Events')}>Events</div>
+      <div className={`${cellStyle} ${ nav === 'Residents' ? navStyle : null }`} onClick={() => setActiveNav('Residents')}>Residents</div>
+      <div className={`${cellStyle} ${ nav === 'Gallery' ? navStyle : null }`} onClick={() => setActiveNav('Gallery')}>Gallery</div>
 
     </nav>
   )
