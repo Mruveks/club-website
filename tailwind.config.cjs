09fd2ea7aa7bc275e1604bd053faa191abd7b705
Kuba Mrowiec <mruvekss@gmail.com>
2023-02-26 13:08:23 +0100

    Bug fix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,15 +8,15 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'resident1': "url('public/resident1.png')",
-        'resident2': "url('public/resident2.png')",
-        'resident3': "url('public/resident3.png')",
-        'resident4': "url('public/resident4.png')",
-        'resident5': "url('public/resident5.png')",
-        'resident6': "url('public/resident6.png')",
-        'resident7': "url('public/resident7.png')",
-        'resident8': "url('public/resident8.png')",
-        'resident9': "url('public/resident9.png')",
+        'resident1': "url('/public/resident1.png')",
+        'resident2': "url('/public/resident2.png')",
+        'resident3': "url('/public/resident3.png')",
+        'resident4': "url('/public/resident4.png')",
+        'resident5': "url('/public/resident5.png')",
+        'resident6': "url('/public/resident6.png')",
+        'resident7': "url('/public/resident7.png')",
+        'resident8': "url('/public/resident8.png')",
+        'resident9': "url('/public/resident9.png')",
       }
     },
   },
