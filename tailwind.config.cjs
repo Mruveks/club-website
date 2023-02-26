2b22f5d1767e900de6f67703304556d64bf4a67c
Kuba Mrowiec <mruvekss@gmail.com>
2023-02-26 13:05:19 +0100

    Bug fix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,20 +2,21 @@
 module.exports = {
   content: [
     "index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/*.{svg, png}"
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'resident1': "url('src/assets/resident1.png')",
-        'resident2': "url('src/assets/resident2.png')",
-        'resident3': "url('src/assets/resident3.png')",
-        'resident4': "url('src/assets/resident4.png')",
-        'resident5': "url('src/assets/resident5.png')",
-        'resident6': "url('src/assets/resident6.png')",
-        'resident7': "url('src/assets/resident7.png')",
-        'resident8': "url('src/assets/resident8.png')",
-        'resident9': "url('src/assets/resident9.png')",
+        'resident1': "url('public/resident1.png')",
+        'resident2': "url('public/resident2.png')",
+        'resident3': "url('public/resident3.png')",
+        'resident4': "url('public/resident4.png')",
+        'resident5': "url('public/resident5.png')",
+        'resident6': "url('public/resident6.png')",
+        'resident7': "url('public/resident7.png')",
+        'resident8': "url('public/resident8.png')",
+        'resident9': "url('public/resident9.png')",
       }
     },
   },
